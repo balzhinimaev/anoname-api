6ab5c0c4afbf5ef29f95398737ec2ce16aff1cb7
alexandr <606620@biletavto.ru>
2025-09-22 22:31:21 +0800

    added log headers

--- src/middleware/botAuth.ts
+++ src/middleware/botAuth.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import config from '../config';
 
 export const botAuth = (req: Request, res: Response, next: NextFunction): void => {
+  console.log(req.headers);
   const secret = req.headers['x-bot-secret'] as string;
   
   if (!secret || secret !== config.botBackendSecret) {
